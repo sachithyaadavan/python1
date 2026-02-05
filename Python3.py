0f1d5e57ff225cb26da1d608aabc413f24a6ce08
sachithyaadavan <ssachiyaadav1405@gmail.com>
2026-02-05 11:58:20 +0530

    ADD py

---
+++ Python3.py
@@ -0,0 +1,12 @@
+firstname="Yogan"
+lastname="Seelan"
+Email="yohaseelan@gmail.com"
+Basicsalary=152000
+
+print("****************************************")
+print  ("\t   EMPLYEE DETAILS")
+print("****************************************")
+print("Full name \t :",firstname , lastname)
+print("Email \t\t :",Email)
+print("Basicsalary \t :",Basicsalary)
+print("****************************************")
