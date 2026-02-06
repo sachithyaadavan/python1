2167e9123ea71b68191b45c768e93c7125d66fdc
sachithyaadavan <ssachiyaadav1405@gmail.com>
2026-02-06 21:30:12 +0530

    Update Python2.py

--- Python2.py
+++ Python2.py
@@ -2,18 +2,30 @@ Studentid=3001
 Studentname="Seelan"
 Grade=12 
 NICnumber=200503758829
+print("***************************************")
+print("***************************************")
 print("Student id is",Studentid)
 print("Studnet name is",Studentname)
 print("Grade is",Grade)
 print("NIC number is",NICnumber)
 print(type(id))
+print("***************************************")
+print("***************************************")
 output=f"Studentid is {Studentid}\nStudentname is {Studentname}\nGrade is {Grade}\nNICnumber is {NICnumber}"
 print(output)
+print("***************************************")
+print("***************************************")
 output="Studentid is{}\nStudentname is {}\nGrade is {}\nNICnumber is {}".format(Studentid,Studentname,Grade,NICnumber)
 print(output)
+print("***************************************")
+print("***************************************")
 output="Studentid is{0}\nStudentname is {1}\nGrade is {2}\nNICnumber is {3}".format(Studentid,Studentname,Grade,NICnumber)
 print(output)
+print("***************************************")
+print("***************************************")
 output=("Studentid is " +str(Studentid)+"\nStudentname is "+ Studentname+"\nGrade is "+str(Grade)+"\nNICnumber is "+str(NICnumber))
 print(output)
+print("***************************************")
+print("***************************************")
 print("Studentid is %d\nStudentname is %s\nGrade is %d\nNICnumber is %d"%(Studentid,Studentname,Grade,NICnumber))
 
