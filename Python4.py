5e0fc938967f2514ee1e989fb051dc8099557754
sachithyaadavan <ssachiyaadav1405@gmail.com>
2026-02-09 10:34:47 +0530

    Add 09/02/2026
    Updated

---
+++ Python4.py
@@ -0,0 +1,6 @@
+name=input("Enter your name : ")
+age=int(input("Enter your age is : "))
+print("Your name is",name)
+print("Your age is",age)
+
+print("Your age is : "+str(age))
---
+++ Python5.py
@@ -0,0 +1,10 @@
+number1=int(input("Enter number 1  : " ))
+number2=int(input("Enter number 2  : " ))
+print("Your first  number is   : ", number1)
+print("Your second number is   : ", number2)
+
+print("Your total value        : " , (number1+number2))
+
+num1=input("Enter num1 : ")
+num2=input("Enter num2 : ")
+print("Toatl value",int(num1)+int(num2))
