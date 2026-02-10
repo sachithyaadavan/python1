41e78a66452ed8ef954d6672e4e6879159d7c677
sachithyaadavan <ssachiyaadav1405@gmail.com>
2026-02-10 21:00:40 +0530

    Added 10/02/2026
    Added on 10/02/2026

---
+++ python9.py
@@ -0,0 +1,20 @@
+name=input("Enter student name")
+age=input("Enter Student age")
+
+a=int(input("Enter Subject 1 :"))
+b=int(input("Enter Subject 2 :"))
+c=int(input("Enter Subject 3 :"))
+d=int(input("Enter Subject 4 :"))
+e=int(input("Enter Subject 5 :"))
+
+print("Student name is",name)
+print("Student age is",age)
+print("Subject 1 :",a)
+print("Subject 2 :",b)
+print("Subject 3 :",c)
+print("Subject 4 :",d)
+print("Subject 5 :",e)
+
+print("TOTAL MARKS OF THE STUDENT :", a+b+c+d+e)
+print("AVERERAGE OF THE STUDENT :", a+b+c+d+e/5)
+
